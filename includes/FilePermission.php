56698d9f221f81ed82ca0680fc21316b60566d85
RazeSoldier <razesoldier@outlook.com>
2018-02-09 21:49:47 +0800

    Let the function library that check file permisson compatible with Windows

--- includes/FilePermission.php
+++ includes/FilePermission.php
@@ -22,57 +22,113 @@
 
 namespace MWExtUpgrader;
 
-/**
- * Checks if file can be create
- * @param string $dir
- * @return boolean
- */
-function createFileAble($dir) {
-	if ( is_writable( $dir ) && is_executable( $dir ) ) {
-		return true;
-	} else {
-		return false;
+# Windows system permission system and linux have a bit different
+if ( PHP_OS === 'WINNT' || PHP_OS === 'Windows' || PHP_OS === 'WIN32') {
+	/**
+	 * Checks if file can be create
+	 * @param string $dir
+	 * @return boolean
+	 */
+	function createFileAble($dir) {
+		if ( is_writable( $dir ) ) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
 
-/**
- * Checks if file can be delete
- * @param string $file
- * @return boolean
- */
-function deleteFileAble($file) {
-	$dir = dirname( $file );
-	if ( is_writable( $dir ) && is_executable( $dir ) && is_writable( $file ) ) {
-		return true;
-	} else {
-		return false;
+	/**
+	 * Checks if file can be delete
+	 * @param string $file
+	 * @return boolean
+	 */
+	function deleteFileAble($file) {
+		$dir = dirname( $file );
+		if ( is_writable( $dir ) && is_writable( $file ) ) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
 
-/**
- * Checks if file can be read
- * @param string $file
- * @return boolean
- */
-function readFileAble($file) {
-	$dir = dirname( $file );
-	if ( is_executable( $dir ) && is_readable( $file ) ) {
-		return true;
-	} else {
-		return false;
+	/**
+	 * Checks if file can be read
+	 * @param string $file
+	 * @return boolean
+	 */
+	function readFileAble($file) {
+		if ( is_readable( $file ) ) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
 
-/**
- * Checks if file can be write
- * @param string $file
- * @return boolean
- */
-function writeFileAble($file) {
-	$dir = dirname( $file );
-	if ( is_executable( $dir ) && is_writable( $file ) ) {
-		return true;
-	} else {
-		return false;
+	/**
+	 * Checks if file can be write
+	 * @param string $file
+	 * @return boolean
+	 */
+	function writeFileAble($file) {
+		if ( is_writable( $file ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+} else {
+	/**
+	 * Checks if file can be create
+	 * @param string $dir
+	 * @return boolean
+	 */
+	function createFileAble($dir) {
+		if ( is_writable( $dir ) && is_executable( $dir ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if file can be delete
+	 * @param string $file
+	 * @return boolean
+	 */
+	function deleteFileAble($file) {
+		$dir = dirname( $file );
+		if ( is_writable( $dir ) && is_executable( $dir ) && is_writable( $file ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if file can be read
+	 * @param string $file
+	 * @return boolean
+	 */
+	function readFileAble($file) {
+		$dir = dirname( $file );
+		if ( is_executable( $dir ) && is_readable( $file ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if file can be write
+	 * @param string $file
+	 * @return boolean
+	 */
+	function writeFileAble($file) {
+		$dir = dirname( $file );
+		if ( is_executable( $dir ) && is_writable( $file ) ) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
